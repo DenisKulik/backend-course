74710a5b0cb45dd88e1c033078d71544941f0549
dkulik <kulikdenis1996x@gmail.com>
2025-01-12 16:06:16 +0700

    feat: testing

---
+++ 05-testing/__tests__/app.test.ts
@@ -0,0 +1,59 @@
+import request from "supertest";
+
+import app from "../src/app";
+import { HttpStatuses } from "../src/app.types";
+
+describe("courses", () => {
+  beforeAll(async () => {
+    await request(app).delete("/__test__/data");
+  });
+
+  it("should return all courses", async () => {
+    const res = await request(app).get("/courses");
+    expect(res.status).toEqual(HttpStatuses.OK);
+    expect(res.body.length).toBe(0);
+  });
+
+  it("shouldn't create a course with incorrect data", async () => {
+    const res = await request(app).post("/courses").send({ title: "" });
+    const db = await request(app).get("/courses");
+    expect(res.status).toEqual(HttpStatuses.BAD_REQUEST);
+    expect(db.body.length).toBe(0);
+  });
+
+  it("should create a course with correct data", async () => {
+    const res = await request(app).post("/courses").send({ title: "test" });
+    const db = await request(app).get("/courses");
+    expect(res.status).toEqual(HttpStatuses.CREATED);
+    expect(res.body.title).toEqual("test");
+    expect(db.body.length).toBe(1);
+  });
+
+  it("should update a course", async () => {
+    const db = await request(app).get("/courses");
+    const courseId = db.body[0].id;
+    const res = await request(app)
+      .put(`/courses/${courseId}`)
+      .send({ title: "test_updated" });
+    expect(res.status).toEqual(HttpStatuses.CREATED);
+    expect(res.body.title).toEqual("test_updated");
+  });
+
+  it("shouldn't update a course with incorrect data", async () => {
+    const db = await request(app).get("/courses");
+    const courseId = db.body[0].id;
+    const res = await request(app)
+      .put(`/courses/${courseId}`)
+      .send({ title: " " });
+    expect(res.status).toEqual(HttpStatuses.BAD_REQUEST);
+  });
+
+  it("should delete a course", async () => {
+    const db = await request(app).get("/courses");
+    const courseId = db.body[0].id;
+    const res = await request(app).delete(`/courses/${courseId}`);
+    expect(res.status).toEqual(HttpStatuses.NO_CONTENT);
+    const dbAfterDelete = await request(app).get("/courses");
+    expect(dbAfterDelete.body.length).toBe(0);
+  });
+});
---
+++ 05-testing/src/app.ts
@@ -0,0 +1,97 @@
+import express, { Express, Request, Response } from "express";
+import { HttpStatuses } from "./app.types";
+
+const app: Express = express();
+const port = 3000;
+
+const jsonBodyMiddleware = express.json();
+app.use(jsonBodyMiddleware);
+
+const db = {
+  courses: [
+    { id: 1, title: "react" },
+    { id: 2, title: "angular" },
+    { id: 3, title: "vue" },
+  ],
+};
+
+app.get("/courses", (req: Request, res: Response) => {
+  let foundCoursesQuery = db.courses;
+
+  if (req.query.title) {
+    foundCoursesQuery = foundCoursesQuery.filter((c) => {
+      return c.title.includes(req.query.title as string);
+    });
+  }
+
+  res.json(foundCoursesQuery);
+});
+
+app.get("/courses/:id", (req: Request, res: Response) => {
+  const foundCourse = db.courses.find((c) => c.id === +req.params.id);
+
+  if (!foundCourse) {
+    res.sendStatus(HttpStatuses.NOT_FOUND).send("Course not found");
+    return;
+  }
+
+  res.status(HttpStatuses.OK).json(foundCourse);
+});
+
+app.post("/courses", (req: Request, res: Response) => {
+  if (!req.body.title || !req.body.title.trim()) {
+    res.sendStatus(HttpStatuses.BAD_REQUEST).send("Title is required");
+    return;
+  }
+
+  const createdCourse = {
+    id: new Date().getTime(),
+    title: req.body.title,
+  };
+  db.courses.push(createdCourse);
+  res.status(HttpStatuses.CREATED).json(createdCourse);
+});
+
+app.delete("/courses/:id", (req: Request, res: Response) => {
+  const foundCourseIdx = db.courses.findIndex((c) => c.id === +req.params.id);
+
+  if (foundCourseIdx === -1) {
+    res.sendStatus(HttpStatuses.NOT_FOUND).send("Course not found");
+    return;
+  }
+
+  db.courses.splice(foundCourseIdx, 1);
+  res.sendStatus(HttpStatuses.NO_CONTENT);
+});
+
+app.put("/courses/:id", (req: Request, res: Response) => {
+  if (!req.body.title || !req.body.title.trim()) {
+    res.sendStatus(HttpStatuses.BAD_REQUEST).send("Title is required");
+    return;
+  }
+
+  const foundCourseIdx = db.courses.findIndex((c) => c.id === +req.params.id);
+
+  if (foundCourseIdx === -1) {
+    res.sendStatus(HttpStatuses.NOT_FOUND).send("Course not found");
+    return;
+  }
+
+  const updatedCourse = {
+    id: db.courses[foundCourseIdx].id,
+    ...req.body,
+  };
+  db.courses[foundCourseIdx] = updatedCourse;
+  res.status(HttpStatuses.CREATED).json(updatedCourse);
+});
+
+app.delete("/__test__/data", (req: Request, res: Response) => {
+  db.courses = [];
+  res.sendStatus(HttpStatuses.NO_CONTENT);
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+export default app;
